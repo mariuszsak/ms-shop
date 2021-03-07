63a7e9d2733726e40cf95c1a0465ef234c11c1c5
mariuszsak <mariuszsak@gmail.com>
2021-03-08 00:19:03 +0100

    First commit

---
+++ apps/webpage/src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/index.css';
+
+class App extends React.Component<{}, {}> {
+    render(): JSX.Element {
+        return (
+            <div>
+                <h1>Welcome to MS Shop</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
