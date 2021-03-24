02ccd01592aa852e64860632003f87f74f5e559e
mariuszsak <mariuszsak@gmail.com>
2021-03-24 13:40:42 +0100

    Minor fixes

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,16 +1,20 @@
 import {PrismaClient} from "@prisma/client";
-import * as express from 'express';
+import express from 'express';
 
 const prisma: any = new PrismaClient();
 
 const app: any = express();
-const port: number = 3000;
+const port = 3000;
 
 app.get('/products', async (req: express.Request, res) => {
-    const products = await prisma.product.findMany();
-    console.log(products);
-    res.send(
-        JSON.stringify(products, (_: string, v: any) => typeof v === 'bigint' ? v.toString() : v));
+    try {
+        const products = await prisma.product.findMany();
+        console.log(products);
+        res.send(
+            JSON.stringify(products, (_: string, v: any) => typeof v === 'bigint' ? v.toString() : v));
+    } catch (e) {
+        console.log(e)
+    }
 });
 
 app.listen(port, () => {
