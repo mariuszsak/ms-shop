66d599660333c87907440b8d4bd5082fade6781f
mariuszsak <mariuszsak@gmail.com>
2021-03-25 23:53:00 +0100

    Added some data to json

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,14 +1,23 @@
 import {PrismaClient} from "@prisma/client";
 import express from 'express';
+import * as path from "path";
 
 const prisma = new PrismaClient();
 
 const app = express();
 const port = 3000;
 
+const dir = path.join(__dirname, '..', 'img')
+app.use('/img', express.static(dir))
+
 app.get('/products', async (req: express.Request, res) => {
     try {
-        const products = await prisma.product.findMany();
+        console.log(path.join(__dirname, '..', 'img'))
+        const products = await prisma.product.findMany({
+            include: {
+                brand: true
+            }
+        });
         res.status(200)
             .send(
                 JSON.stringify(products, (_: string, v: any) => typeof v === 'bigint' ? v.toString() : v));
